084bfa5549ca8b87644293477944ca3db6c5a514
pratik-dabhi <pratik.dabhiii.23@gmail.com>
2024-07-20 17:19:18 +0530

    developed login & seo comp

---
+++ src/components/seo/Seo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ISeo } from '../../interfaces/interfaces';
+import { Helmet } from 'react-helmet';
+
+const Seo = ({title,metaDescription}:ISeo) => {
+  return (
+        <Helmet>‍
+          <title>{title}</title>‍
+          <meta name="description" content={metaDescription} />        
+        </Helmet>
+  )
+}
+
+export default Seo
